22ad0d4cafcff9779acb71e8173f0e0defc8785d
atulnitrr <atulkumar458@gmail.com>
2019-07-29 21:24:40 +0530

    Added tooltip

--- src/component/layout/AddBtn.js
+++ src/component/layout/AddBtn.js
@@ -5,7 +5,9 @@ const AddBtn = () => {
     <div className="fixed-action-btn">
       <a
         href="#add_group_modal"
-        className="btn-floating btn-large modal-trigger blue darken-2"
+        className="btn-floating btn-large modal-trigger blue darken-2 tooltipped"
+        data-position="left"
+        data-tooltip="Add group"
       >
         <i className="large material-icons">add</i>
       </a>
@@ -14,7 +16,9 @@ const AddBtn = () => {
         <li>
           <a
             href="#add_bill"
-            className="btn-floating btn-large modal-trigger blue darken-2"
+            className="btn-floating btn-large modal-trigger blue darken-2 tooltipped"
+            data-position="left"
+            data-tooltip="Record payment"
           >
             <i className="large material-icons">payment</i>
           </a>
@@ -22,7 +26,9 @@ const AddBtn = () => {
         <li>
           <a
             href="#add_person_to_group"
-            className="btn-floating btn-large modal-trigger blue darken-2"
+            className="btn-floating btn-large modal-trigger blue darken-2 tooltipped"
+            data-position="left"
+            data-tooltip="Add user in group"
           >
             <i className="large material-icons">person_add</i>
           </a>
