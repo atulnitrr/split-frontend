8007a23f819511f888141bc79b591238e385b9c3
atulnitrr <atulkumar458@gmail.com>
2019-07-31 00:05:24 +0530

    Initialize materialize css on groups

--- src/component/usergroup/UserGroup.js
+++ src/component/usergroup/UserGroup.js
@@ -1,10 +1,14 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import AddBtn from "../layout/AddBtn";
 import AddGroupModal from "./AddGroupModal";
 import AddPersonModal from "./AddPersonModal";
 import AddPaymentModal from "./AddPaymentModal";
+import M from "materialize-css/dist/js/materialize.min.js";
 
 const UserGroup = () => {
+  useEffect(() => {
+    M.AutoInit();
+  });
   return (
     <Fragment>
       <AddBtn />
